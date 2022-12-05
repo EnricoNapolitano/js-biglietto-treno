05b24955901afca0a9592428b55e0b8efe104683
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2022-12-05 18:08:47 +0100

    adding again condition "else"

--- script.js
+++ script.js
@@ -36,30 +36,31 @@ const userAge = parseInt(prompt("Quanti anni hai?", 33).trim());
 //* 4
 if (!userKms || !userAge) {
     alert("Valore non valido. Per piacere, inserisci dei valori numerici")
+} else {
+    //* 5.
+    //Price for km
+    const unitPrice = 0.21;
+    
+    //User kms Price
+    let price = unitPrice * userKms;
+    
+    //Discount under 18 & over 65
+    const discountYoungPeople = price * 0.2;
+    const discountOldPoeple = price * 0.4;
+    
+    // // console.log(unitPrice, price, discountYoungPeople, discountOldPoeple);
+    
+    if (userAge < 18){
+        price += - discountYoungPeople;
+    } else if (userAge > 64) {
+        price += - discountOldPoeple;
+    }
+    
+    // // console.log(price);
+    
+    let formatPrice = price.toFixed(2);
+    
+    //* 6.
+    targetElement.innerHTML = `<h1>Il prezzo del tuo biglietto è pari a <b> ${formatPrice} &euro; </b></h1>`;
 }
 
-//* 5.
-//Price for km
-const unitPrice = 0.21;
-
-//User kms Price
-let price = unitPrice * userKms;
-
-//Discount under 18 & over 65
-const discountYoungPeople = price * 0.2;
-const discountOldPoeple = price * 0.4;
-
-// // console.log(unitPrice, price, discountYoungPeople, discountOldPoeple);
-
-if (userAge < 18){
-    price += - discountYoungPeople;
-} else if (userAge > 64) {
-    price += - discountOldPoeple;
-}
-
-// // console.log(price);
-
-let formatPrice = price.toFixed(2);
-
-//* 6.
-targetElement.innerHTML = `<h1>Il prezzo del tuo biglietto è pari a <b> ${formatPrice} &euro; </b></h1>`;
